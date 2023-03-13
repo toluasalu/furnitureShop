df3f3f9a49bc45f3270b4ec052ca130d13addd21
toluasalu <johnasalu13@gmail.com>
2023-03-14 00:14:58 +0100

    Just the detail screen left to work on

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,8 +1,12 @@
+import React,  { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, 
+       TextInput, TouchableOpacity, FlatList, Dimensions, Image, Pressable } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import Colors from './constants/colors';
+import furnitures from "./constants/furnitures";
 
+const {width} = Dimensions.get("screen");
 
 const categoryItems = [
     { name: "Chair", iconName: "seat-outline" },
@@ -15,17 +19,77 @@ const categoryItems = [
 const HomeScreen = ({ navigation }) => {
 
     const ListCategories = () => {
+        const [ selCategoryIndex, setSelCategoryIndex ] = useState(0);
+
         return (
             <View style={styles.categoriesContainer}>
                 {categoryItems.map((item, index) => (
-                    <View style={[styles.itemCategoryBtn]}>
-                        <MaterialCommunityIcons name={item.iconName} size={20} color={Colors.primary} />
-                        <Text style={[]}>{item.name}</Text>
+                <TouchableOpacity
+                    activeOpacity={0.8} 
+                    key={index}
+                    onPress ={() => setSelCategoryIndex(index)}>
+                    <View style={[styles.itemCategoryBtn, {backgroundColor: selCategoryIndex == index ? Colors.primary: Colors.white}]}>
+                    <MaterialCommunityIcons name={item.iconName} size={20} color={selCategoryIndex == index ? Colors.white : Colors.primary} />
+                    <Text style={[styles.categoryText, {color: selCategoryIndex == index ? Colors.white : Colors.primary } ]}>{item.name}</Text>
                     </View>
+                    </TouchableOpacity>
+                  
                 ))}
             </View>
         )
     }
+
+      const Card = ({furniture}) => {
+          return (
+           <Pressable onPress={() => navigation.navigate("Details", furniture)}>
+             <View style={styles.card}>
+             <View style={styles.iconContainer}>
+               <MaterialCommunityIcons name="heart" color={furniture.liked ? Colors.red : Colors.primary}/>
+            </View>
+             <Image source={furniture.image} style={{
+                height: 120, 
+                width: "100%", 
+                borderRadius: 10}} />
+
+               <Text style={styles.cardName}>{furniture.name}</Text>
+                <View style={{marginTop: 5, flexDirection: "row", justifyContent: "space-between"}}>
+                    <Text style={styles.price}>{furniture.price}</Text>
+                    <View style={{flexDirection: "row", marginLeft: 10}}>
+                      <MaterialCommunityIcons name="star" color={Colors.yellow} size={18} />
+                      <Text style={styles.rating}> 4.3</Text>
+                    </View>
+                </View>
+             </View>
+          </Pressable>
+          )
+           
+      }
+
+      const ItemCard = ({furniture}) => {
+        return(
+            <View style={styles.itemCard}>
+                <Image source={furniture.image} style={{
+                    width: 100, 
+                    height: "100%", 
+                    borderTopLeftRadius: 10, 
+                    borderBottomLeftRadius: 10,
+                    marginRight: 10,
+
+                    }}/>
+                    <View style={{paddingVertical: 15, justifyContent: "center"}}>
+                        <Text style={styles.cardName}>{furniture.name}</Text>
+                        <View style={{flexDirection: "row", marginTop:10}}>
+                            <Text style={styles.price}>{furniture.price}</Text>
+                          <View style={{flexDirection: "row", marginLeft: 10}}>
+                             <MaterialCommunityIcons name="star" color={Colors.yellow} size={18}/>
+                             <Text style={styles.rating}>4.3</Text>
+                           </View>
+                        </View>
+                       
+                    </View>
+            </View>
+        )
+      }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -68,6 +132,22 @@ const HomeScreen = ({ navigation }) => {
 
                 <Text style={styles.decText}>Categories</Text>
                 <ListCategories />
+                <Text style={styles.decText}> Top Furniture</Text>
+                <FlatList 
+                contentContainerStyle={{paddingLeft: 20}}
+                horizontal 
+                showsHorizontalScrollIndicator={false}
+                data={furnitures}
+                renderItem={({item}) => <Card furniture ={item} />}
+                />
+                <Text style={styles.decText}>Popular</Text>
+                <FlatList 
+                contentContainerStyle={{paddingLeft: 20}}
+                horizontal 
+                showsHorizontalScrollIndicator={false}
+                data={furnitures}
+                renderItem={({item}) => <ItemCard furniture ={item} />}
+                />
             </ScrollView>
             <StatusBar />
         </SafeAreaView>
@@ -93,7 +173,8 @@ const styles = StyleSheet.create({
     decText: {
         fontSize: 18,
         fontWeight: "bold",
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
+        paddingVertical: 10
     },
     categoriesContainer: {
         flexDirection: "row",
@@ -107,7 +188,67 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 7,
         alignItems: "center"
-    }
+    },
+    categoryText: {
+        fontSize: 13,
+        fontWeight: "bold",
+        color: Colors.primary,
+        marginLeft: 5,
+    },
+
+    card: {
+        height: 190,
+        backgroundColor: Colors.white,
+        elevation: 10,
+        width: width/2.5,
+        marginRight: 20,
+        padding: 10,
+        marginVertical: 20,
+        borderRadius: 10
+    }, 
+
+    cardName: {
+        marginTop:10,
+        fontSize: 12,
+        color: Colors.primary,
+        fontWeight: "bold"
+    },
+
+    price: {
+        fontSize: 12,
+        color: Colors.primary,
+        fontWeight: "bold"
+    },
+
+    rating: {
+        color: Colors.primary,
+        fontWeight: "bold",
+        fontSize: 12
+    },
+    itemCard: {
+      height: 90,
+      width: -100,
+      backgroundColor: Colors.white,
+      elevation: 10,
+      marginVertical: 20,
+      marginRight: 20,
+      borderRadius: 10,
+      flexDirection: "row"
+
+    },
+    iconContainer: {
+      height: 25,
+      width: 25,
+      backgroundColor: Colors.white,
+      position: "absolute",
+      elevation: 2,
+      right: 15,
+      top: 15,
+      borderRadius: 15,
+      justifyContent: "center",
+      alignItems: "center"
+
+    } 
 });
 
 
